729c014928dc9081db1c67a3f50b6e6754df54ec
sournachos <joserivera18100@gmail.com>
2021-11-10 17:47:06 -0600

    Got some mobile view work. Will work on backend/database starting tomorrow

--- client/src/components/formulaire.js
+++ client/src/components/formulaire.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { Fragment, useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
@@ -5,13 +6,13 @@ import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
 const people = [
   {
     id: 1,
-    name: 'Jose Rivera',
+    name: 'Luis Rivera',
     avatar:
       'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     id: 2,
-    name: 'Jose Rivera',
+    name: 'Antonio Rivera',
     avatar:
       'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
@@ -23,43 +24,43 @@ const people = [
   },
   {
     id: 4,
-    name: 'Jose Rivera',
+    name: 'Mario Rivera',
     avatar:
       'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     id: 5,
-    name: 'Jose Rivera',
+    name: 'Luigi Rivera',
     avatar:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     id: 6,
-    name: 'Jose Rivera',
+    name: 'Lucas Rivera',
     avatar:
       'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     id: 7,
-    name: 'Jose Rivera',
+    name: 'Ruby Rivera',
     avatar:
       'https://images.unsplash.com/photo-1568409938619-12e139227838?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     id: 8,
-    name: 'Jose Rivera',
+    name: 'Bambi Rivera',
     avatar:
       'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     id: 9,
-    name: 'Jose Rivera',
+    name: 'Lizzy Rivera',
     avatar:
       'https://images.unsplash.com/photo-1584486520270-19eca1efcce5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     id: 10,
-    name: 'Jose Rivera',
+    name: 'Steph Rivera',
     avatar:
       'https://images.unsplash.com/photo-1561505457-3bcad021f8ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
@@ -71,6 +72,12 @@ function classNames(...classes) {
 
 export default function Formulaire() {
   const [selected, setSelected] = useState(people[3])
+
+  useEffect(() => {
+
+    console.log(selected.name)
+    
+  }, [selected])
 
   return (
     <Listbox value={selected} onChange={setSelected}>
--- client/src/components/navvy.js
+++ client/src/components/navvy.js
@@ -4,8 +4,8 @@ function Navvy() {
     return (
         <nav className="menu">
             <ol>
-                <li className="menu-item"><a href="#0">Home</a></li>
-                <li className="menu-item"><a href="#0">About</a></li>
+                <li className="menu-item"><a href="/">Home</a></li>
+                <li className="menu-item"><a href="#0">Info</a></li>
             </ol>
         </nav>
     );
